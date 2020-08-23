ad5b514eea9d71a6e601882dee2cbf612fdc9bef
masoud momen <masoudmomen@hotmail.com>
2020-08-23 14:53:45 +0430

    first commit for start calculator project

---
+++ app/src/main/java/com/example/firstprj/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.firstprj
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Toast.makeText(this, "Hello World!!!", Toast.LENGTH_LONG).show()
+//        txtMain.text = "salam"
+//        findViewById<Button>(R.id.btn).setOnClickListener{
+//            Toast.makeText(this, "You are press the button", Toast.LENGTH_LONG).show()
+//        }
+    }
+}
