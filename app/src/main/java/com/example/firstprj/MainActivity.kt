c7c87a14ff7562f2f9d8fcfe447cff9fc0d79377
masoud momen <masoudmomen@hotmail.com>
2020-08-23 19:09:33 +0430

    complete sum action

--- app/src/main/java/com/example/firstprj/MainActivity.kt
+++ app/src/main/java/com/example/firstprj/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.firstprj
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -11,10 +12,93 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toast.makeText(this, "Hello World!!!", Toast.LENGTH_LONG).show()
-//        txtMain.text = "salam"
-//        findViewById<Button>(R.id.btn).setOnClickListener{
-//            Toast.makeText(this, "You are press the button", Toast.LENGTH_LONG).show()
-//        }
+        button0.setOnClickListener(View.OnClickListener {
+            val txt = mainTxt.text
+            val txt1 = StringBuilder()
+            txt1.append(txt).append("0")
+            mainTxt.setText(txt1)
+        })
+
+        button1.setOnClickListener(View.OnClickListener {
+            val txt = mainTxt.text
+            val txt1 = StringBuilder()
+            txt1.append(txt).append("1")
+            mainTxt.setText(txt1)
+        })
+        button2.setOnClickListener(View.OnClickListener {
+            val txt = mainTxt.text
+            val txt1 = StringBuilder()
+            txt1.append(txt).append("2")
+            mainTxt.setText(txt1)
+        })
+        button3.setOnClickListener(View.OnClickListener {
+            val txt = mainTxt.text
+            val txt1 = StringBuilder()
+            txt1.append(txt).append("3")
+            mainTxt.setText(txt1)
+        })
+        button4.setOnClickListener(View.OnClickListener {
+            val txt = mainTxt.text
+            val txt1 = StringBuilder()
+            txt1.append(txt).append("4")
+            mainTxt.setText(txt1)
+        })
+        button5.setOnClickListener(View.OnClickListener {
+            val txt = mainTxt.text
+            val txt1 = StringBuilder()
+            txt1.append(txt).append("5")
+            mainTxt.setText(txt1)
+        })
+        button6.setOnClickListener(View.OnClickListener {
+            val txt = mainTxt.text
+            val txt1 = StringBuilder()
+            txt1.append(txt).append("6")
+            mainTxt.setText(txt1)
+        })
+        button7.setOnClickListener(View.OnClickListener {
+            val txt = mainTxt.text
+            val txt1 = StringBuilder()
+            txt1.append(txt).append("7")
+            mainTxt.setText(txt1)
+        })
+        button8.setOnClickListener(View.OnClickListener {
+            val txt = mainTxt.text
+            val txt1 = StringBuilder()
+            txt1.append(txt).append("8")
+            mainTxt.setText(txt1)
+        })
+        button9.setOnClickListener(View.OnClickListener {
+            val txt = mainTxt.text
+            val txt1 = StringBuilder()
+            txt1.append(txt).append("9")
+            mainTxt.setText(txt1)
+        })
+        buttonSum.setOnClickListener(View.OnClickListener {
+            val txt = mainTxt.text
+            val txt1 = StringBuilder()
+            txt1.append(txt).append("+")
+            mainTxt.setText(txt1)
+        })
+        buttonSubmission.setOnClickListener(View.OnClickListener {
+            val txt = mainTxt.text
+            val txt1 = StringBuilder()
+            txt1.append(txt).append("-")
+            mainTxt.setText(txt1)
+        })
+        buttonMultiplication.setOnClickListener(View.OnClickListener {
+            val txt = mainTxt.text
+            val txt1 = StringBuilder()
+            txt1.append(txt).append("*")
+            mainTxt.setText(txt1)
+        })
+
+        buttonEqual.setOnClickListener(View.OnClickListener {
+            val txt = mainTxt.text
+            val txtArray = txt?.split("+")
+            val number = txtArray?.get(0)?.toInt()?.plus(txtArray?.get(1)?.toInt())
+            val txt1 = StringBuilder()
+            txt1.append(txt).append("=").append(number)
+            mainTxt.setText(txt1)
+        })
     }
 }
